3eadc48b73fbc9d7b53ea59bb8308c97bbcbc6d8
root <root@node-2.domain.tld>
2015-04-21 13:37:27 +0000

    Cluster templates

--- lib/openstack/sahara/connection.rb
+++ lib/openstack/sahara/connection.rb
@@ -56,6 +56,50 @@ module OpenStack
         true
       end
 
+############################################################################
+
+      def cluster_template_url(cluster_template_id = nil)
+        url = '/cluster-templates'
+        url += "/#{cluster_template_id}" if cluster_template_id
+        url
+      end
+
+      def list_cluster_templates
+        response = connection.req('GET', cluster_template_url)
+        cluster_templates_hash = JSON.parse(response.body)['cluster_templates']
+        cluster_templates_hash.inject([]) { |res, current| res << OpenStack::Sahara::Cluster_Template.new(current); res}
+      end
+
+      def get_cluster_template(cluster_template_id)
+        response = connection.req('GET', cluster_template_url(cluster_template_id))
+        cluster_template_hash = JSON.parse(response.body)['cluster_template']
+        return unless cluster_template_hash
+        OpenStack::Sahara::Cluster_Template.new cluster_template_hash
+      end
+
+      def create_cluster_template(options)
+        # check input data
+        data = JSON.generate(options)
+        response = connection.csreq('POST',
+                                     connection.service_host,
+                                     "#{connection.service_path}#{cluster_template_url}",
+                                     connection.service_port,
+                                     connection.service_scheme,
+                                     {
+                                         'content-type' => 'application/json',
+                                     },
+                                     data
+        )
+        OpenStack::Exception.raise_exception(response) unless response.code.match(/^20.$/)
+        cluster_template_info = JSON.parse(response.body)['cluster_template']
+        OpenStack::Volume::Volume.new cluster_template_info
+      end
+
+      def delete_cluster_template(cluster_template_id)
+        connection.req('DELETE', cluster_template_url(cluster_template_id))
+        true
+      end
+
     end
 
   end
